0e5adaca236374cf59ad88c5d906ee8328fc5a2c
Denyse <mdenyse15@gmail.com>
2022-01-16 12:10:05 +0200

    Feat: (Added testing by role)

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -6,3 +6,9 @@ it("should render same text passrd into title prop", () => {
   const headingElement = screen.getByText(/my header/i);
   expect(headingElement).toBeInTheDocument();
 });
+
+it("should get element by role", () => {
+  render(<Header title="My Header" />);
+  const headingElement = screen.getByRole(/heading/i);
+  expect(headingElement).toBeInTheDocument();
+});
