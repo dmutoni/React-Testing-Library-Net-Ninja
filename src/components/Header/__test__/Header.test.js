3f784a1a644e7bd48e869f3b8fc6a5856958fb93
Denyse <mdenyse15@gmail.com>
2022-01-16 12:36:34 +0200

    Feat: (Added testing by test id and title)

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -12,8 +12,22 @@ it("should render same text passed into title prop", () => {
 //   const headingElement = screen.getByRole(/heading/i);
 //   expect(headingElement).toBeInTheDocument();
 // });
-it("should get element by role", () => {
+it("should get element by role", async () => {
   render(<Header title="My Header" />);
   const headingElement = screen.getByRole("heading", { name: "My Header" });
   expect(headingElement).toBeInTheDocument();
 });
+
+it("should get by title", async () => {
+  render(<Header title="My Header" />);
+  const headingElement = screen.getByTitle("Header");
+  expect(headingElement).toBeInTheDocument();
+});
+
+it("should get by id", async () => {
+  render(<Header title="My Header" />);
+  const headingElement = screen.getByTestId("header-2");
+  expect(headingElement).toBeInTheDocument();
+});
+
+// FIND BY
