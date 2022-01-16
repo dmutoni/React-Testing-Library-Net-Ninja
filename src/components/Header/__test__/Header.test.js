8bb40e02db2d87fb113de0fe23dab42c33166d18
Denyse <mdenyse15@gmail.com>
2022-01-16 12:23:17 +0200

    Feat: (Added getting by element and specify the text)

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -1,14 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import Header from "../Header";
 
-it("should render same text passrd into title prop", () => {
+it("should render same text passed into title prop", () => {
   render(<Header title="My Header" />);
   const headingElement = screen.getByText(/my header/i);
   expect(headingElement).toBeInTheDocument();
 });
 
+// it("should get element by role", () => {
+//   render(<Header title="My Header" />);
+//   const headingElement = screen.getByRole(/heading/i);
+//   expect(headingElement).toBeInTheDocument();
+// });
 it("should get element by role", () => {
   render(<Header title="My Header" />);
-  const headingElement = screen.getByRole(/heading/i);
+  const headingElement = screen.getByRole("heading", { name: "My Header" });
   expect(headingElement).toBeInTheDocument();
 });
